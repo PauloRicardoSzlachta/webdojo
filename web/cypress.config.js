89c432bfabd55b5fdac62d597b17e18b9b7b1110
Paulo Ricardo Szlachta <kadutks@unochapeco.edu.br>
2026-03-28 22:12:31 -0300

    Aplicada alteração para rodar no Cloud do Cypress

--- web/cypress.config.js
+++ web/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
-
+  projectId: "65tbu6",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
